875115c96755b59b688e305175ab0692e82069da
Scott Kenealy <kenescott@gmail.com>
2017-08-05 14:42:17 -0500

    Initial Commit

---
+++ Eject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Eject
+//
+//  Created by Scott Kenealy on 8/5/17.
+//  Copyright © 2017 Scott Kenealy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
